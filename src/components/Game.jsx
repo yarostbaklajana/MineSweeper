da443a1edeb7c24083af67957467343946943b2e
Volodymyr Osmak <darkxahtep@gmail.com>
2016-07-06 04:24:02 +0300

    Added skeleton for Game component (#2)

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,39 @@
-import {Link} from 'react-router';
 import React from 'react';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
+
+const textStyles = {
+    paddingRight: '5px'
+}
+
+const iconStyles = {
+    paddingLeft: 0
+}
+
+const separatorStyles = {
+    margin: '0 5px 0 0'
+}
 
 const Game = (props) => (
     <div>
-        <Link to="/MineSweeper/">Pause</Link>
-        <div>Hello game!</div>
+        <Toolbar>
+            <ToolbarGroup firstChild={true}>
+                <RaisedButton icon={<FontIcon className="fa fa-pause" />} primary={true}/>
+            </ToolbarGroup>
+            <ToolbarGroup lastChild={true}>
+                <FontIcon style={iconStyles} className="fa fa-bomb"/>
+                <ToolbarTitle style={textStyles} text={3 + "/" + 10}/>
+                <ToolbarSeparator style={separatorStyles}/>
+                <FontIcon style={iconStyles} className="fa fa-clock-o"/>
+                <ToolbarTitle style={textStyles} text={ 123}/>
+            </ToolbarGroup>
+        </Toolbar>
+        <div className="board-container">
+            <div className="center-helper">
+                <div className="board"></div>
+            </div>
+        </div>
     </div>
 )
 
